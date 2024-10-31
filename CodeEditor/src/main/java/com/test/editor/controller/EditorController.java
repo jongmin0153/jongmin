5d4448665eb36ad3998748db6ef48202a1ec725b
chimy2 <premet@naver.com>
2024-11-01 00:59:05 +0900

    Add project settings

---
+++ CodeEditor/src/main/java/com/test/editor/controller/EditorController.java
@@ -0,0 +1,13 @@
+package com.test.editor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EditorController {
+	
+	@GetMapping("/")
+	public String test() {
+		return "test";
+	}
+}
