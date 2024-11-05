94378d7582c783d0d4dadfaad89bdeeb1c3e334a
fjhgf <fjhgf@naver.com>
2024-11-05 19:21:00 +0900

    Add join css code

--- CodeEditor/src/main/resources/static/js/main.js
+++ CodeEditor/src/main/resources/static/js/main.js
@@ -1,40 +1,24 @@
-
-/*
-const header = document.querySelectorAll('.headerImg img');
-
-header[0].onclick = function(event) {
-	alert(event.target);
-	//마이페이지로 이동
-};
-
-header[1].onclick = function(event) {
-	alert(event.target);
-	//개인 설정 창이 떠야 함 > child 창
-};
-*/
-
-/*main -> join*/
 document.querySelector('.logo').onclick = function(){
-	location.href = '/editor/main'
+	location.href = '/editor/main';
 }
 
-const loginbox = document.querySelectorAll('.login_box');
+const loginbox = document.querySelectorAll('.login_header_box');
 loginbox[0].onclick = function(){
-	location.href = '/editor/join'
+	location.href = '/editor/join';
 }
 
 loginbox[1].onclick = function(){
-	location.href = '/editor/login'
+	location.href = '/editor/login';
+}
+
+document.querySelector('.join_box').onclick = function(){
+	document.querySelector('.email_join').style.display = 'block';
 }
 
 
-
-
-
-
-
-
-
+document.querySelectorAll('.join_button button')[1].onclick = function(){
+	document.querySelector('.email_join').style.display = 'none';
+}
 
 
 
